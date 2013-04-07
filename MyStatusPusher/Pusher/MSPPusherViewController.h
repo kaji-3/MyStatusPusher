36fd2e083d1a537c63a30ad0b927d92a9d052229
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-04-07 22:42:36 +0900

    MapViewの実現

--- MyStatusPusher/Pusher/MSPPusherViewController.h
+++ MyStatusPusher/Pusher/MSPPusherViewController.h
@@ -2,12 +2,16 @@
 //  MSPPusherViewController.h
 //  MyStatusPusher
 //
-//  Created by y_kajikawa on 13/04/06.
+//  Created by y_kajikawa on 13/04/07.
 //  Copyright (c) 2013年 y_kajikawa. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface MSPPusherViewController : UITableViewController
+@interface MSPPusherViewController : UIViewController <CLLocationManagerDelegate>
+{
+    CLLocationManager *_locationManager;
+}
 
 @end
--- MyStatusPusher/Pusher/MSPPusherViewController.m
+++ MyStatusPusher/Pusher/MSPPusherViewController.m
@@ -2,11 +2,13 @@
 //  MSPPusherViewController.m
 //  MyStatusPusher
 //
-//  Created by y_kajikawa on 13/04/06.
+//  Created by y_kajikawa on 13/04/07.
 //  Copyright (c) 2013年 y_kajikawa. All rights reserved.
 //
 
 #import "MSPPusherViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
 @interface MSPPusherViewController ()
 
@@ -14,11 +16,13 @@
 
 @implementation MSPPusherViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithStyle:style];
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        
+        
     }
     return self;
 }
@@ -26,96 +30,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    // 位置情報の取得
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+    
+    if ([CLLocationManager locationServicesEnabled]) {
+        [_locationManager startUpdatingLocation];
+    }
+}
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+// 位置情報更新時に呼ばれるハンドラ
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation{
+    
+    // 表示範囲の指定
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.005f, 0.005f);
+    
+    // 更新された位置をマップの中心に設定
+    CLLocationCoordinate2D coordinate = newLocation.coordinate;
+    MKCoordinateRegion region = MKCoordinateRegionMake(coordinate, span);
+    MKMapView *mapView = (MKMapView*) self.view;
+    [mapView setRegion:region animated:true];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
 }
 
 @end
