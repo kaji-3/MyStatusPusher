1cdd1c0a698fa23640b8931bf36b7149bc46cefe
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-04-14 13:07:33 +0900

    MapView への位置情報反映実装完了

--- MyStatusPusher/Pusher/MSPPusherViewController.m
+++ MyStatusPusher/Pusher/MSPPusherViewController.m
@@ -50,12 +50,54 @@
     // 更新された位置をマップの中心に設定
     CLLocationCoordinate2D coordinate = newLocation.coordinate;
     MKCoordinateRegion region = MKCoordinateRegionMake(coordinate, span);
-    MKMapView *mapView = (MKMapView*) self.view;
+    MKMapView *mapView = (MKMapView*)[self.view viewWithTag:4];
+    UITextView *textView = (UITextView*)[self.view viewWithTag:2];
+    
     [mapView setRegion:region animated:true];
     
     // 非同期通信で特定サーバ
-    
+    dispatch_queue_t globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+    dispatch_async(globalQueue, ^{
+        
+        //時間のかかる処理
+        int count = 0, i;
+        for(i = 0; i < 10000; i++){
+            NSLog(@"count = %d", count);
+            count++;
+        }
+        
+        //メインスレッドで途中結果表示
+        dispatch_async(mainQueue, ^{
+            [self appendLog:[self logStringWithCount:count]];
+        });
+        
+        //時間のかかる処理
+        for(i = 0; i < 10000; i++){
+            NSLog(@"count = %d", count);
+            count++;
+        }
+        
+        //メインスレッドで終了処理
+        dispatch_async(mainQueue, ^{
+            [self appendLog:[self logStringWithCount:count]];
+            //[_indicatorView stopAnimating];
+            //_startButton.hidden = NO;
+        });
+    });
 }
+
+- (NSString *)logStringWithCount:(int)count
+{
+    return [NSString stringWithFormat:@"count is %d", count];
+}
+
+- (void)appendLog:(NSString *)log
+{
+    //_logView.text = [NSString stringWithFormat:@"%@\n%@", _logView.text, log];
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {
